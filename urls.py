efc417be54ef6a40c38ba0dcd341596303f07be9
sachinjangili <sachinjangili@gmail.com>
2026-02-17 15:48:06 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,32 @@
+"""
+URL configuration for jobportal project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from portal import views
+
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('admin/', admin.site.urls),
+    path('api/', include('portal.urls')),
+    path("login/", views.login_view, name='login'),
+    path("logout/", views.logout_view, name='logout'),
+    path("dashboard/", views.dashboard, name='dashboard'),
+    path("profile/", views.profile_view, name='profile'),
+    path("post-job/", views.post_job, name='post_job'),
+    path('register/', views.register_view, name='register'),
+    path('apply/<int:job_id>/', views.apply_job, name='apply_job'),
+]
